1a4a33257cf28e6bab03ececa6cc5ecca88c1ca4
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2020-02-16 15:37:43 +0100

    feat(ListService): add logic

--- angular-src/src/app/list.service.ts
+++ angular-src/src/app/list.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
+import { List } from "./models/list";
+import { Observable } from "rxjs";
+
+const apiUrl = "http://localhost:3000";
 
 @Injectable({
   providedIn: "root"
 })
 export class ListService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  public getAllLists(): Observable<List[]> {
+    const url = `${apiUrl}/bucketlist/`;
+    return this.http
+      .get<{ lists: List[] }>(url)
+      .pipe(map(response => response.lists));
+  }
+
+  public deleteList(listId: string) {
+    const url = `${apiUrl}/bucketlist/${listId}`;
+    return this.http.delete(url);
+  }
+
+  public addList(list: List): Observable<{ success: boolean }> {
+    const url = `${apiUrl}/bucketlist/`;
+    return this.http.post<{ success: boolean }>(url, list);
+  }
 }
