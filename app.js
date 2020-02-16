084abc56a954269b3a4ffcd48b8187216fd28e22
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2020-02-16 14:09:36 +0100

    feat(*): apply prettier to update formatting

--- app.js
+++ app.js
@@ -6,38 +6,37 @@ const mongoose = require("mongoose");
 const config = require("./config/database");
 const bucketlist = require("./controllers/bucketlist");
 
-//Connect mongoose to our database
+// Connect mongoose to our database
 mongoose.connect(config.database, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-//Declaring Port
+// Declaring Port
 const port = 3000;
 
-//Initialize our app variable
+// Initialize our app variable
 const app = express();
 
-//Middleware for CORS
+// Middleware for CORS
 app.use(cors());
 
 //Middlewares for bodyparsing using both json and urlencoding
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-/*express.static is a built in middleware function to serve static files.
- We are telling express server public folder is the place to look for the static files
-*/
+// express.static is a built in middleware function to serve static files.
+// We are telling express server public folder is the place to look for the static files
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("Invalid page");
 });
 
-//Routing all HTTP requests to /bucketlist to bucketlist controller
+// Routing all HTTP requests to /bucketlist to bucketlist controller
 app.use("/bucketlist", bucketlist);
 
-//Listen to port 3000
+// Listen to port 3000
 app.listen(port, () => {
   console.log(`Starting the server at port ${port}`);
 });
