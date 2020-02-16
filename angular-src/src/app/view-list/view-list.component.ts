dc15fdcf1ee39351cac413766e0ddb0df36f1d3f
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2020-02-16 15:37:58 +0100

    feat(ViewListComponent): add logic

--- angular-src/src/app/view-list/view-list.component.ts
+++ angular-src/src/app/view-list/view-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { List } from "../models/list";
+import { ListService } from "../list.service";
 
 @Component({
   selector: "app-view-list",
@@ -6,7 +8,30 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./view-list.component.css"]
 })
 export class ViewListComponent implements OnInit {
-  constructor() {}
+  public lists: List[] = [];
 
-  ngOnInit(): void {}
+  constructor(private listService: ListService) {}
+
+  ngOnInit(): void {
+    this.loadLists();
+  }
+
+  // Get all lists from server and update the lists property
+  private loadLists() {
+    this.listService.getAllLists().subscribe(lists => {
+      this.lists = lists;
+    });
+  }
+
+  // The deleted list is being filtered out using the .filter method
+  public deleteList(listToDelete: List) {
+    this.listService.deleteList(listToDelete._id).subscribe(() => {
+      this.lists = this.lists.filter(list => list !== listToDelete);
+    });
+  }
+
+  // onAddList will be invoked when the child component emits an event
+  public onAddList(newList) {
+    this.loadLists();
+  }
 }
