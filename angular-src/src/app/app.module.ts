4a8feb1199c5c7e6b13feff70d3e1b0375a8023e
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2020-02-16 14:29:38 +0100

    feat(angular-src): add AddListComponent, ViewListComponent and ListService

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddListComponent } from './add-list/add-list.component';
+import { ViewListComponent } from './view-list/view-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddListComponent,
+    ViewListComponent
   ],
   imports: [
     BrowserModule
