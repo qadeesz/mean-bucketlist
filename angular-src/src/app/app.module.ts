301e0a1cb065864851309df3fbd39693c343e51b
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2020-02-16 14:39:43 +0100

    feat(AppModule): import FormsModule and HttpClientModule

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { AddListComponent } from "./add-list/add-list.component";
 import { ViewListComponent } from "./view-list/view-list.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent, AddListComponent, ViewListComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
